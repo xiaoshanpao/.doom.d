2ace02dd54149d303a0174ecefaca074f56d6fc6
fanhe <xiaoming.east@gmail.com>
2020-12-19 00:19:36 +0800

    [ADD] 打开emacs时最大化

--- config.el
+++ config.el
@@ -52,3 +52,6 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; Maximize when start
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
